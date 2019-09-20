1b93a2550710ef33be36075bf45d16ff93ee17ab
Lars Kristoffer Jørgensen <larskrj@Larss-MBP.shdir.no>
2019-09-20 08:59:56 +0200

    (split)Lagt til tester for script tag validering #209289

---
+++ src/components/formcomponents/string/__tests__/string-spec.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { String } from '../string';
+import { QuestionnaireItem } from '../../../../types/fhir';
+import { mount } from 'enzyme';
+
+describe('string', () => {
+  describe('When input has html and validateScriptInjection = true', () => {
+    const validateScriptInjection = true;
+    const value = 'input med <html>';
+    it('Should render with validation', () => {
+      const wrapper = getStringWrapperWithChangedValue(validateScriptInjection, value);
+
+      expect(wrapper.html()).toContain('&lt;html&gt; er ikke tillatt');
+    });
+  });
+
+  describe('When input does not have html and validateScriptInjection = true', () => {
+    const validateScriptInjection = true;
+    const value = 'input med uten html';
+    it('Should render without validation', () => {
+      const wrapper = getStringWrapperWithChangedValue(validateScriptInjection, value);
+
+      expect(wrapper.html()).not.toContain('er ikke tillatt');
+    });
+  });
+
+  describe('When input has html and validateScriptInjection = false', () => {
+    const validateScriptInjection = false;
+    const value = 'input med <html>';
+    it('Should render with validation', () => {
+      const wrapper = getStringWrapperWithChangedValue(validateScriptInjection, value);
+
+      expect(wrapper.html()).not.toContain('er ikke tillatt');
+    });
+  });
+});
+
+function getStringWrapperWithChangedValue(validateScriptInjection: boolean, value: string) {
+  const item: QuestionnaireItem = {
+    id: '2',
+    linkId: '2.1',
+    repeats: false,
+    type: 'string',
+    text: 'Uten html',
+  };
+  const wrapper = mount(
+    <String
+      item={item}
+      answer={{} as any}
+      path={[]}
+      id="item_2"
+      oneToTwoColumn
+      validateScriptInjection={validateScriptInjection}
+      repeatButton={<div />}
+      renderDeleteButton={() => {
+        return undefined;
+      }}
+      visibleDeleteButton
+      renderHelpButton={() => <React.Fragment />}
+      renderHelpElement={() => <React.Fragment />}
+    />
+  );
+  wrapper.update();
+  const input = wrapper.find('input');
+  input.simulate('change', { target: { value } });
+  input.simulate('blur');
+  wrapper.update();
+  return wrapper;
+}
