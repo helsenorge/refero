6ec78056a3537cbe5833ebdafacb63ea0ff6fd07
Eirik Søreng <eirik.soreng@nhn.no>
2024-01-04 09:50:03 +0100

    Fix unit test setup for supporting StructuredClone
    - To make unit tests support StructuredClone
    - Updates Jest versions and simplify jest setup to whats neccessary for node 18
    - The structured clone fix is needed to fix error: "StructuredCopy is not defined"
     *  Ended up using ungap npm package to resolve this:
     *     1) It it is very similar to structuredClone ( which is needed for some of the tests that do actual deep copies )
     *     2) Avoids using a hybrid of jest test environments and node for different tests
     *     3) The ungap package seems stable ( lot of downloads )
     No possible to just mock this method, since unit tests rely on this method to work in several tests.

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,5 +1,3 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 const root = path.resolve(__dirname, '../');
 
