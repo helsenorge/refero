d3cd2775c27edaef59efc90af2e908715dd593da
Astrid Bakken <astrid.bakken@nhn.no>
2024-01-11 09:00:49 +0100

    test: add test for no extention on attachment

--- src/components/formcomponents/attachment/__tests__/attachment-spec.tsx
+++ src/components/formcomponents/attachment/__tests__/attachment-spec.tsx
@@ -2,7 +2,7 @@ import { QuestionnaireItem } from '../../../../types/fhir';
 import { getMaxSizeExtensionValue } from '../../../../util/extension';
 
 describe('Attachment form component', () => {
-  const question: QuestionnaireItem = {
+  const questionExtention: QuestionnaireItem = {
     linkId: '4c71df6e-d743-46ba-d81f-f62777ffddb4',
     type: 'attachment',
     text: '5mb',
@@ -13,9 +13,17 @@ describe('Attachment form component', () => {
       },
     ],
   };
-  it('Should give back right value in MB',
-    () => {
-      const test = getMaxSizeExtensionValue(question);
-      expect(test).toBe(5);
-    });
+  it('Should give back right value from extention in MB', () => {
+    const test = getMaxSizeExtensionValue(questionExtention);
+    expect(test).toBe(5);
+  });
+  const question: QuestionnaireItem = {
+    linkId: '4c71df6e-d743-46ba-d81f-f62777ffddb4',
+    type: 'attachment',
+    text: 'No extention',
+  };
+  it('Should fail to undefined without extention', () => {
+    const test = getMaxSizeExtensionValue(question);
+    expect(test).toBe(undefined);
+  });
 });
