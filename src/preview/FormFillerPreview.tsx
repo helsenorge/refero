831902bc6422821c2e78fffca15eadb6305f1bf7
rinrub <99617143+rinrub@users.noreply.github.com>
2024-02-28 11:57:34 +0100

    added styling for help icon

--- src/preview/FormFillerPreview.tsx
+++ src/preview/FormFillerPreview.tsx
@@ -10,7 +10,7 @@ import LanguageLocales from '@helsenorge/core-utils/constants/languages';
 import FormFillerSidebar from './FormFillerSidebar';
 import { emptyPropertyReplacer } from './helpers';
 import { getResources } from './resources/referoResources';
-import skjema from './skjema/NHNTestSlider (3).json';
+import skjema from './skjema/kun_qest.json';
 import { ReferoContainer } from '../components';
 import rootReducer from '../reducers';
 
