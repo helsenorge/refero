492caa67c51b3fdd38c5f929329a1d48149c6179
rinrub <99617143+rinrub@users.noreply.github.com>
2024-06-11 16:15:36 +0200

    rename

--- src/components/formcomponents/text/__tests__/__data__/index.ts
+++ src/components/formcomponents/text/__tests__/__data__/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import { Questionnaire } from 'fhir/r4';
 
-const qinline: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/qinline.json').toString());
+const qinline: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/inline.json').toString());
 const q: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q.json').toString());
 const qScriptInjection: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/scriptInjection.json').toString());
 
