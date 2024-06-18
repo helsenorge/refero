dfe5813b1b86a7016e1cd7f99cc5ae98a3258350
rinrub <99617143+rinrub@users.noreply.github.com>
2024-06-18 15:26:11 +0200

    fix small lint errors

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,6 @@
-import { combineReducers, Reducer } from 'redux';
+import { CombinedState, combineReducers, Reducer } from 'redux';
+import { FormAction } from 'src/actions/form';
+import { NewValueAction } from 'src/actions/newValue';
 
 import form, { Form } from '../reducers/form';
 
@@ -10,7 +12,14 @@ export interface ReferoState {
   form: Form;
 }
 // Benyttes kun for tester eller hvis Refero kjøres utenfor helsenorge
-const rootReducer: Reducer<{}> = combineReducers({
+const rootReducer: Reducer<
+  CombinedState<{
+    refero: CombinedState<{
+      form: Form | undefined;
+    }>;
+  }>,
+  NewValueAction | FormAction
+> = combineReducers({
   refero: combineReducers({ form }),
 });
 
