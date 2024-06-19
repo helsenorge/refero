a3ee05493f48208a43470bfc95ab40020dd116b0
rinrub <99617143+rinrub@users.noreply.github.com>
2024-06-19 17:59:10 +0200

    update readme remove engines from package.json, fix tslint error

--- src/util/group-item-control.ts
+++ src/util/group-item-control.ts
@@ -4,22 +4,6 @@ import { getItemControlExtensionValue } from './extension';
 import itemControlConstants from '../constants/itemcontrol';
 import itemType from '../constants/itemType';
 
-export function getGroupItemControl(item: QuestionnaireItem): Coding[] {
-  if (item.type !== itemType.GROUP) return [];
-
-  const itemControl = getItemControlExtensionValue(item);
-  if (!itemControl) return [];
-
-  const groups = itemControlConstants.Group;
-  const valid = Object.keys(groups).map(k => groups[k]);
-  return itemControl.filter(i => valid.indexOf(i.code) != -1);
-}
-/*
- import { QuestionnaireItem, Coding } from 'fhir/r4';
-import { getItemControlExtensionValue } from './extension';
-import itemControlConstants from '../constants/itemcontrol';
-import itemType from '../constants/itemType';
-
 type GroupKeys = keyof typeof itemControlConstants.Group;
 type GroupValues = (typeof itemControlConstants.Group)[GroupKeys];
 
@@ -37,5 +21,3 @@ export function getGroupItemControl(item: QuestionnaireItem): Coding[] {
     return valid.has(i.code as GroupValues);
   });
 }
-
- */
