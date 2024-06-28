9406c9c56614ef4b21d1cd8428174eede40e13ce
rinrub <99617143+rinrub@users.noreply.github.com>
2024-06-28 10:16:21 +0200

    add tests

--- src/validation/__tests__/__data__/index.ts
+++ src/validation/__tests__/__data__/index.ts
@@ -2,6 +2,10 @@ import * as fs from 'fs';
 import { Questionnaire } from 'fhir/r4';
 
 const q: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q.json').toString());
-const string: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/string.json').toString());
-
-export { q, string };
+const qString: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/qString.json').toString());
+const qInteger: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/qInteger.json').toString());
+const qDecimal: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/qDecimal.json').toString());
+const qText: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/qText.json').toString());
+const qQuantity: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/qQuantity.json').toString());
+const qBoolean: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/qBoolean.json').toString());
+export { q, qString, qInteger, qDecimal, qText, qQuantity, qBoolean };
--- src/validation/__tests__/defaultFormValues-spec.ts
+++ src/validation/__tests__/defaultFormValues-spec.ts
@@ -1,5 +1,5 @@
 import { Questionnaire } from 'fhir/r4';
-import { string as qString } from './__data__';
+import { qString, qInteger, qDecimal, qText, qQuantity, qBoolean } from './__data__';
 import { ReferoProps } from '@/types/referoProps';
 import { renderRefero } from '@test/test-utils';
 import { getResources } from '../../../preview/resources/referoResources';
@@ -12,22 +12,47 @@ describe('Default form values', () => {
       expect(createIntitialFormValues()).toEqual({});
     });
   });
-  describe('Initial decimal value', () => {});
-  describe('Initial integer value', () => {});
-  describe('Initial date value', () => {});
-  describe('Initial dateTime value', () => {});
-  describe('Initial time value', () => {});
-  describe('Initial choice value', () => {
+  describe('Initial decimal value', () => {
+    it('prefilled values should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qDecimal);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Preutfylt decimal/i)).not.toBeInTheDocument();
+    });
+    it('empty fields should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qDecimal);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Tom decimal/i)).toBeInTheDocument();
+    });
+  });
+  describe('Initial integer value', () => {
+    it('prefilled values should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qInteger);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Preutfylt integer/i)).not.toBeInTheDocument();
+    });
+    it('empty fields should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qInteger);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Tom integer/i)).toBeInTheDocument();
+    });
+  });
+  describe.skip('Initial date value', () => {});
+  describe.skip('Initial dateTime value', () => {});
+  describe.skip('Initial time value', () => {});
+  describe.skip('Initial choice value', () => {
     describe('initial Radiobutton value', () => {});
     describe('initial checkbox value', () => {});
     describe('initial dropdown value', () => {});
+    describe('initial autosuggest value', () => {});
   });
-  describe('Initial open choice value', () => {
+  describe.skip('Initial open choice value', () => {
     describe('initial Radiobutton value', () => {});
     describe('initial checkbox value', () => {});
     describe('initial dropdown value', () => {});
+    describe('initial autosuggest value', () => {});
+    describe('initial radiobutton extra-field value', () => {});
   });
-  describe.only('Initial string value', () => {
+  describe('Initial string value', () => {
     it('prefilled values should not cause a validation error on submit', async () => {
       const wrapper = createWrapper(qString);
       await submitForm();
@@ -39,10 +64,43 @@ describe('Default form values', () => {
       expect(wrapper.queryByTestId(/summary-element-Tom string/i)).toBeInTheDocument();
     });
   });
-  describe('Initial boolean value', () => {});
-  describe('Initial attachment value', () => {});
-  describe('Initial quantity value', () => {});
-  describe('Initial text value', () => {});
+  describe('Initial boolean value', () => {
+    it('prefilled values should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qBoolean);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Preutfylt boolean/i)).not.toBeInTheDocument();
+    });
+    it('empty fields should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qBoolean);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Tom boolean/i)).toBeInTheDocument();
+    });
+  });
+  describe.skip('Initial attachment value', () => {});
+  describe('Initial quantity value', () => {
+    it('prefilled values should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qQuantity);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Preutfylt quantity/i)).not.toBeInTheDocument();
+    });
+    it('empty fields should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qQuantity);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Tom quantity/i)).toBeInTheDocument();
+    });
+  });
+  describe('Initial text value', () => {
+    it('prefilled values should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qText);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Preutfylt text/i)).not.toBeInTheDocument();
+    });
+    it('empty fields should not cause a validation error on submit', async () => {
+      const wrapper = createWrapper(qText);
+      await submitForm();
+      expect(wrapper.queryByTestId(/summary-element-Tom text/i)).toBeInTheDocument();
+    });
+  });
 });
 const createWrapper = (questionnaire: Questionnaire, props: Partial<ReferoProps> = {}) => {
   return renderRefero({ questionnaire, props: { ...props, resources } });
