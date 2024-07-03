f842bf6479173b1eac4157f0bef49ec594c882c1
anagelpe <andreas.nagel-petersen@nhn.no>
2024-07-03 10:09:11 +0200

    Made tests for date day

--- src/components/formcomponents/date/__tests__/date-spec.tsx
+++ src/components/formcomponents/date/__tests__/date-spec.tsx
@@ -1,33 +1,392 @@
-import { QuestionnaireItem, QuestionnaireResponseItemAnswer } from 'fhir/r4';
-import { createDateFromYear } from '../../../../util/createDateFromYear';
+import { Questionnaire, QuestionnaireResponseItemAnswer } from 'fhir/r4';
+import { act, findByRole, renderRefero, userEvent } from '@test/test-utils.tsx';
+import { q } from './__data__';
+import { ReferoProps } from '../../../../types/referoProps';
+import { Extensions } from '../../../../constants/extensions';
+import { clickButtonTimes, submitForm } from '../../../../../test/selectors';
+import { getResources } from '../../../../../preview/resources/referoResources';
+import { vi } from 'vitest';
 
-describe.skip('Date form component', () => {
-  const question: QuestionnaireItem = {
-    id: '2',
-    linkId: '2.1',
-    repeats: false,
-    type: 'group',
-    text: 'Overskrift',
-  };
-  it('Should parse the date-year-input to string correctly', () => {
-    const answer: QuestionnaireResponseItemAnswer = {
-      linkId: '123',
-      valueDate: '2005',
-      answer: [
-        {
-          valueString: 'abc',
-          item: [
+const resources = { ...getResources(''), formRequiredErrorMessage: 'Du må fylle ut dette feltet', oppgiGyldigVerdi: 'ikke gyldig tall' };
+
+describe('Date day', () => {
+  describe('Render', () => {
+    it('Should render as text if props.pdf', () => {
+      const { queryByText } = createWrapper(q, { pdf: true });
+      expect(queryByText('Ikke besvart')).toBeInTheDocument();
+    });
+    it('Should render text if item is readonly', () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, readOnly: true })),
+      };
+      const { queryByText } = createWrapper(questionnaire);
+      expect(queryByText('Ikke besvart')).toBeInTheDocument();
+    });
+    it('Should render as input if props.pdf === false && item is not readonly', () => {
+      const { queryByText } = createWrapper(q);
+      expect(queryByText('Ikke besvart')).not.toBeInTheDocument();
+    });
+  });
+  describe('initialvalue', () => {
+    it('Initial value should not be set', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({
+          ...x,
+          repeats: false,
+        })),
+      };
+      const { getByLabelText } = createWrapper(questionnaire);
+
+      expect(getByLabelText(/Dato/i)).toHaveValue('');
+    });
+
+    it('Initial value should be set', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({
+          ...x,
+          repeats: false,
+          initial: [
             {
-              linkId: '789',
+              valueDate: '31.05.1994',
             },
           ],
-        },
-        {
-          valueString: 'def',
-        },
-      ],
-    } as QuestionnaireResponseItemAnswer;
-    const test = createDateFromYear(question, answer);
-    expect(test?.toISOString()).toBe('2005-01-01T00:00:00.000Z');
+        })),
+      };
+      const { getByLabelText } = createWrapper(questionnaire);
+
+      expect(getByLabelText(/Dato/i)).toHaveValue('31.05.1994');
+    });
   });
+  describe('help button', () => {
+    it('Should render helpButton', async () => {
+      const { container } = createWrapper(q);
+
+      expect(container.querySelector('.page_refero__helpButton')).toBeInTheDocument();
+    });
+    it('Should render helpElement when helpbutton is clicked', async () => {
+      const { container } = createWrapper(q);
+
+      expect(container.querySelector('.page_refero__helpButton')).toBeInTheDocument();
+
+      expect(container.querySelector('.page_refero__helpComponent--open')).not.toBeInTheDocument();
+
+      const helpButton = container.querySelector('.page_refero__helpButton');
+      if (helpButton) {
+        await act(async () => {
+          userEvent.click(helpButton);
+        });
+      }
+
+      expect(container.querySelector('.page_refero__helpComponent--open')).toBeInTheDocument();
+    });
+  });
+  describe('repeat button', () => {
+    it('Should render repeat button if item repeats', () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+
+      const { getByTestId } = createWrapper(questionnaire);
+      const repeatButton = getByTestId(/-repeat-button/i);
+      expect(repeatButton).toBeInTheDocument();
+    });
+
+    it('Should not render repeat button if item does not repeats', () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: false })),
+      };
+      const { queryByTestId } = createWrapper(questionnaire);
+      const repeatButton = queryByTestId(/-repeat-button/i);
+      expect(repeatButton).not.toBeInTheDocument();
+    });
+    it('Should add item when repeat is clicked and remove button when maxOccurance(4) is reached', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+        extension: q.extension?.map(y => {
+          if (y.url === Extensions.MIN_OCCURS_URL) {
+            return { ...y, valueInteger: 2 };
+          }
+          return y;
+        }),
+      };
+      const { queryAllByLabelText, queryByTestId, debug } = createWrapper(questionnaire);
+      await clickButtonTimes(/-repeat-button/i, 3);
+
+      // debug(undefined, 50000);
+      expect(queryAllByLabelText(/Dato/i)).toHaveLength(4);
+      expect(queryByTestId(/-repeat-button/i)).not.toBeInTheDocument();
+    });
+  });
+  describe('delete button', () => {
+    it('Should render delete button if item repeats and number of repeated items is greater than minOccurance(2)', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { queryAllByTestId } = createWrapper(questionnaire);
+
+      await clickButtonTimes(/-repeat-button/i, 2);
+
+      expect(queryAllByTestId(/-delete-button/i)).toHaveLength(2);
+    });
+    it('Should not render delete button if item repeats and number of repeated items is lower or equal than minOccurance(2)', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { queryByTestId } = createWrapper(questionnaire);
+
+      expect(queryByTestId(/-delete-button/i)).not.toBeInTheDocument();
+    });
+    it('Should show confirmationbox when deletebutton is clicked', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { getByTestId } = createWrapper(questionnaire);
+
+      await clickButtonTimes(/-repeat-button/i, 1);
+
+      expect(getByTestId(/-delete-button/i)).toBeInTheDocument();
+      await clickButtonTimes(/-delete-button/i, 1);
+
+      expect(getByTestId(/-delete-confirm-modal/i)).toBeInTheDocument();
+    });
+    it('Should remove item when delete button is clicked', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { getByTestId, queryByTestId } = createWrapper(questionnaire);
+
+      await clickButtonTimes(/-repeat-button/i, 1);
+
+      expect(getByTestId(/-delete-button/i)).toBeInTheDocument();
+      await clickButtonTimes(/-delete-button/i, 1);
+
+      const confirmModal = getByTestId(/-delete-confirm-modal/i);
+      await act(async () => {
+        userEvent.click(await findByRole(confirmModal, 'button', { name: /Forkast endringer/i }));
+      });
+
+      expect(queryByTestId(/-delete-button/i)).not.toBeInTheDocument();
+    });
+  });
+  // describe('onChange', () => {
+  //   it('Should update component with value from answer', async () => {
+  //     const { getByLabelText } = createWrapper(q);
+
+  //     const inputElement = getByLabelText(/Decimal/i);
+  //     expect(inputElement).toBeInTheDocument();
+  //     expect(inputElement).toHaveAttribute('type', 'number');
+  //     expect(inputElement).toHaveAttribute('id', `item_${q?.item?.[0].linkId}^0`);
+  //     await act(async () => {
+  //       userEvent.type(inputElement, '123');
+  //     });
+  //     expect(getByLabelText(/Decimal/i)).toHaveValue(123);
+  //   });
+  //   it('Should call onChange with correct value', async () => {
+  //     const onChange = vi.fn();
+  //     const { getByLabelText } = createWrapper(q, { onChange });
+  //     expect(getByLabelText(/Decimal/i)).toBeInTheDocument();
+  //     await act(async () => {
+  //       userEvent.type(getByLabelText(/Decimal/i), '1.2');
+  //     });
+  //     const expectedAnswer: QuestionnaireResponseItemAnswer = {
+  //       valueDecimal: 1.2,
+  //     };
+  //     expect(onChange).toHaveBeenCalledTimes(2);
+  //     expect(onChange).toHaveBeenCalledWith(expect.any(Object), expectedAnswer, expect.any(Object), expect.any(Object));
+  //   });
+  // });
+  describe('Validation', () => {
+    describe('Required', () => {
+      it('Should show error if field is required and value is empty', async () => {
+        const questionnaire: Questionnaire = {
+          ...q,
+          item: q.item?.map(x => ({ ...x, required: true })),
+        };
+        const { getByText } = createWrapper(questionnaire);
+        await submitForm();
+
+        expect(getByText(resources.formRequiredErrorMessage)).toBeInTheDocument();
+      });
+      it('Should not show error if required and has value', async () => {
+        const questionnaire: Questionnaire = {
+          ...q,
+          item: q.item?.map(x => ({ ...x, required: true })),
+        };
+        const { getByLabelText, queryByText } = createWrapper(questionnaire);
+        await act(async () => {
+          userEvent.type(getByLabelText(/Dato/i), '31.05.1994');
+        });
+        await submitForm();
+
+        expect(queryByText(resources.formRequiredErrorMessage)).not.toBeInTheDocument();
+      });
+      // it('Should remove error on change if form is submitted', async () => {
+      //   const questionnaire: Questionnaire = {
+      //     ...q,
+      //     item: q.item?.map(x => ({ ...x, required: true })),
+      //   };
+      //   const { getByText, queryByText, getByLabelText } = createWrapper(questionnaire);
+      //   await submitForm();
+      //   expect(getByText(resources.formRequiredErrorMessage)).toBeInTheDocument();
+
+      //   await act(async () => {
+      //     userEvent.type(getByLabelText(/Dato/i), '31.05.1994');
+      //     userEvent.tab();
+      //   });
+      //   expect(queryByText(resources.formRequiredErrorMessage)).not.toBeInTheDocument();
+      // });
+    });
+  });
+  //   describe('maxValue', () => {
+  //     it('Should not show error if value is empty', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({ ...x, required: false })),
+  //       };
+  //       const { queryByText } = createWrapper(questionnaire);
+  //       await submitForm();
+
+  //       expect(queryByText('Custom error')).not.toBeInTheDocument();
+  //     });
+  //     it('Should not show error if value is bellow max value (10) and over min(5)', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({ ...x, required: false })),
+  //       };
+  //       const { getByLabelText, queryByText } = createWrapper(questionnaire);
+  //       await act(async () => {
+  //         userEvent.type(getByLabelText(/Decimal/i), '8');
+  //       });
+  //       await submitForm();
+
+  //       expect(queryByText('Custom error')).not.toBeInTheDocument();
+  //     });
+  //     it('Should remove error on change if form is submitted', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({ ...x, required: false })),
+  //       };
+  //       const { getByText, queryByText, getByLabelText } = createWrapper(questionnaire);
+  //       await act(async () => {
+  //         userEvent.type(getByLabelText(/Decimal/i), '12');
+  //       });
+  //       await submitForm();
+  //       expect(getByText('Custom error')).toBeInTheDocument();
+  //       await act(async () => {
+  //         userEvent.clear(getByLabelText(/Decimal/i));
+  //         userEvent.type(getByLabelText(/Decimal/i), '8');
+  //       });
+
+  //       expect(queryByText('Custom error')).not.toBeInTheDocument();
+  //     });
+  //   });
+  //   describe('minValue', () => {
+  //     it('Should not show error if value is empty', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({ ...x, required: false })),
+  //       };
+  //       const { queryByText } = createWrapper(questionnaire);
+  //       await submitForm();
+
+  //       expect(queryByText('Custom error')).not.toBeInTheDocument();
+  //     });
+  //     it('Should not show error if value is bellow max value (10) and over min(5)', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({ ...x, required: false })),
+  //       };
+  //       const { getByLabelText, queryByText } = createWrapper(questionnaire);
+  //       await act(async () => {
+  //         userEvent.type(getByLabelText(/Decimal/i), '8');
+  //       });
+  //       await submitForm();
+
+  //       expect(queryByText('Custom error')).not.toBeInTheDocument();
+  //     });
+  //     it('Should remove error on change if form is submitted', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({ ...x, required: false })),
+  //       };
+  //       const { queryByText, getByLabelText } = createWrapper(questionnaire);
+  //       await act(async () => {
+  //         userEvent.type(getByLabelText(/Decimal/i), '3');
+  //       });
+  //       await submitForm();
+  //       expect(queryByText('Custom error')).toBeInTheDocument();
+  //       await act(async () => {
+  //         userEvent.clear(getByLabelText(/Decimal/i));
+  //         userEvent.type(getByLabelText(/Decimal/i), '8');
+  //       });
+
+  //       expect(queryByText('Custom error')).not.toBeInTheDocument();
+  //     });
+  //   });
+  //   describe('decimalPattern validation', () => {
+  //     it('Should not show error if value is empty', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({
+  //           ...x,
+  //           required: false,
+  //         })),
+  //       };
+  //       const { queryByText } = createWrapper(questionnaire);
+  //       await submitForm();
+
+  //       expect(queryByText(resources.oppgiGyldigVerdi)).not.toBeInTheDocument();
+  //     });
+  //     it('Should not show error if value is valid pattern', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({
+  //           ...x,
+  //           required: false,
+  //         })),
+  //       };
+  //       const { getByLabelText, queryByText } = createWrapper(questionnaire);
+  //       await act(async () => {
+  //         userEvent.type(getByLabelText(/Decimal/i), '6.12');
+  //       });
+  //       await submitForm();
+
+  //       expect(queryByText(resources.oppgiGyldigVerdi)).not.toBeInTheDocument();
+  //     });
+  //     it('Should remove error on change if form is submitted', async () => {
+  //       const questionnaire: Questionnaire = {
+  //         ...q,
+  //         item: q.item?.map(x => ({
+  //           ...x,
+  //           required: false,
+  //         })),
+  //       };
+  //       const { queryByText, getByLabelText } = createWrapper(questionnaire);
+  //       await act(async () => {
+  //         userEvent.type(getByLabelText(/Decimal/i), '6.121212');
+  //       });
+  //       await submitForm();
+  //       expect(queryByText(resources.oppgiGyldigVerdi)).toBeInTheDocument();
+  //       await act(async () => {
+  //         userEvent.clear(getByLabelText(/Decimal/i));
+  //         userEvent.type(getByLabelText(/Decimal/i), '6.12');
+  //       });
+
+  //       expect(queryByText(resources.oppgiGyldigVerdi)).not.toBeInTheDocument();
+  //     });
+  //   });
 });
+
+const createWrapper = (questionnaire: Questionnaire, props: Partial<ReferoProps> = {}) => {
+  return renderRefero({ questionnaire, props: { ...props, resources } });
+};
