0c16438fc9f3a2aae2e10ad24c47a7bcfde3545a
anagelpe <andreas.nagel-petersen@nhn.no>
2024-07-08 14:55:48 +0200

    Renamed questionnaire json files for date-day tests

--- src/components/formcomponents/date/__tests__/__data__/index.ts
+++ src/components/formcomponents/date/__tests__/__data__/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import { Questionnaire } from 'fhir/r4';
 
-const q: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q.json').toString());
-const qMinMax: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-minMax.json').toString());
-const qMinMaxCustomError: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-minMax-customError.json').toString());
+const q: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-dateDay.json').toString());
+const qMinMax: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-dateDay-minMax.json').toString());
+const qMinMaxCustomError: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-dateDay-minMax-customError.json').toString());
 export { q, qMinMax, qMinMaxCustomError };
--- src/components/formcomponents/date/__tests__/date-day-spec.tsx
+++ src/components/formcomponents/date/__tests__/date-day-spec.tsx
@@ -1,5 +1,5 @@
 import { Questionnaire, QuestionnaireResponseItemAnswer } from 'fhir/r4';
-import { act, findByRole, queryByText, renderRefero, userEvent } from '@test/test-utils.tsx';
+import { act, findByRole, renderRefero, userEvent } from '@test/test-utils.tsx';
 import { q, qMinMax, qMinMaxCustomError } from './__data__';
 import { ReferoProps } from '../../../../types/referoProps';
 import { Extensions } from '../../../../constants/extensions';
