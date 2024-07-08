ef4aca50879a5724966a2b6199d8e64b21167f04
anagelpe <andreas.nagel-petersen@nhn.no>
2024-07-08 15:09:33 +0200

    Created tests for date-time

---
+++ src/components/formcomponents/date/__tests__/date-time-spec.tsx
@@ -0,0 +1,190 @@
+import { Questionnaire, QuestionnaireResponseItemAnswer } from 'fhir/r4';
+import { act, findByRole, renderRefero, userEvent } from '@test/test-utils.tsx';
+import { q } from './__data__/date-time';
+import { ReferoProps } from '../../../../types/referoProps';
+import { Extensions } from '../../../../constants/extensions';
+import { clickButtonTimes, submitForm } from '../../../../../test/selectors';
+import { getResources } from '../../../../../preview/resources/referoResources';
+import { vi } from 'vitest';
+
+const resources = {
+  ...getResources(''),
+  formRequiredErrorMessage: 'Du må fylle ut dette feltet',
+  oppgiGyldigVerdi: 'ikke gyldig tall',
+  dateError_invalid: 'Ugyldig dato',
+  errorBeforeMinDate: 'Dato kan ikke være før minimums dato',
+  errorAfterMaxDate: 'Dato kan ikke være etter maksimum dato',
+};
+
+describe('Date day', () => {
+  describe('Render', () => {
+    it('Should render as text if props.pdf', () => {
+      const { queryByText } = createWrapper(q, { pdf: true });
+      expect(queryByText('Ikke besvart')).toBeInTheDocument();
+    });
+    it('Should render text if item is readonly', () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, readOnly: true })),
+      };
+      const { queryByText } = createWrapper(questionnaire);
+      expect(queryByText('Ikke besvart')).toBeInTheDocument();
+    });
+    it('Should render as input if props.pdf === false && item is not readonly', () => {
+      const { queryByText } = createWrapper(q);
+      expect(queryByText('Ikke besvart')).not.toBeInTheDocument();
+    });
+  });
+  describe('initialvalue', () => {
+    it('Initial value should not be set', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({
+          ...x,
+          repeats: false,
+        })),
+      };
+      const { getByLabelText } = createWrapper(questionnaire);
+
+      expect(getByLabelText(/Dato/i)).toHaveValue('');
+    });
+    it('Initial value should be set', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({
+          ...x,
+          repeats: false,
+          initial: [
+            {
+              valueDateTime: '31.05.1994',
+            },
+          ],
+        })),
+      };
+      const { getByLabelText } = createWrapper(questionnaire);
+
+      expect(getByLabelText(/Dato/i)).toHaveValue('31.05.1994');
+    });
+  });
+  describe('help button', () => {
+    it('Should render helpButton', async () => {
+      const { container } = createWrapper(q);
+
+      expect(container.querySelector('.page_refero__helpButton')).toBeInTheDocument();
+    });
+    it('Should render helpElement when helpbutton is clicked', async () => {
+      const { container } = createWrapper(q);
+
+      expect(container.querySelector('.page_refero__helpButton')).toBeInTheDocument();
+
+      expect(container.querySelector('.page_refero__helpComponent--open')).not.toBeInTheDocument();
+
+      const helpButton = container.querySelector('.page_refero__helpButton');
+      if (helpButton) {
+        await act(async () => {
+          userEvent.click(helpButton);
+        });
+      }
+
+      expect(container.querySelector('.page_refero__helpComponent--open')).toBeInTheDocument();
+    });
+  });
+  describe('repeat button', () => {
+    it('Should render repeat button if item repeats', () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+
+      const { getByTestId } = createWrapper(questionnaire);
+      const repeatButton = getByTestId(/-repeat-button/i);
+      expect(repeatButton).toBeInTheDocument();
+    });
+
+    it('Should not render repeat button if item does not repeats', () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: false })),
+      };
+      const { queryByTestId } = createWrapper(questionnaire);
+      const repeatButton = queryByTestId(/-repeat-button/i);
+      expect(repeatButton).not.toBeInTheDocument();
+    });
+    it('Should add item when repeat is clicked and remove button when maxOccurance(4) is reached', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+        extension: q.extension?.map(y => {
+          if (y.url === Extensions.MIN_OCCURS_URL) {
+            return { ...y, valueInteger: 2 };
+          }
+          return y;
+        }),
+      };
+      const { queryAllByLabelText, queryByTestId } = createWrapper(questionnaire);
+      await clickButtonTimes(/-repeat-button/i, 3);
+
+      expect(queryAllByLabelText(/Dato/i)).toHaveLength(4);
+      expect(queryByTestId(/-repeat-button/i)).not.toBeInTheDocument();
+    });
+  });
+  describe('delete button', () => {
+    it('Should render delete button if item repeats and number of repeated items is greater than minOccurance(2)', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { queryAllByTestId } = createWrapper(questionnaire);
+
+      await clickButtonTimes(/-repeat-button/i, 2);
+
+      expect(queryAllByTestId(/-delete-button/i)).toHaveLength(2);
+    });
+    it('Should not render delete button if item repeats and number of repeated items is lower or equal than minOccurance(2)', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { queryByTestId } = createWrapper(questionnaire);
+
+      expect(queryByTestId(/-delete-button/i)).not.toBeInTheDocument();
+    });
+    it('Should show confirmationbox when deletebutton is clicked', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { getByTestId } = createWrapper(questionnaire);
+
+      await clickButtonTimes(/-repeat-button/i, 1);
+
+      expect(getByTestId(/-delete-button/i)).toBeInTheDocument();
+      await clickButtonTimes(/-delete-button/i, 1);
+
+      expect(getByTestId(/-delete-confirm-modal/i)).toBeInTheDocument();
+    });
+    it('Should remove item when delete button is clicked', async () => {
+      const questionnaire: Questionnaire = {
+        ...q,
+        item: q.item?.map(x => ({ ...x, repeats: true })),
+      };
+      const { getByTestId, queryByTestId } = createWrapper(questionnaire);
+
+      await clickButtonTimes(/-repeat-button/i, 1);
+
+      expect(getByTestId(/-delete-button/i)).toBeInTheDocument();
+      await clickButtonTimes(/-delete-button/i, 1);
+
+      const confirmModal = getByTestId(/-delete-confirm-modal/i);
+      await act(async () => {
+        userEvent.click(await findByRole(confirmModal, 'button', { name: /Forkast endringer/i }));
+      });
+
+      expect(queryByTestId(/-delete-button/i)).not.toBeInTheDocument();
+    });
+  });
+});
+
+const createWrapper = (questionnaire: Questionnaire, props: Partial<ReferoProps> = {}) => {
+  return renderRefero({ questionnaire, props: { ...props, resources } });
+};
