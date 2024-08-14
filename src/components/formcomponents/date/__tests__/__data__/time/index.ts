5b425aa3d49ffdfc0cd2f482decd7024aba855ee
anagelpe <andreas.nagel-petersen@nhn.no>
2024-08-14 15:52:56 +0200

    Created tests for pdf in time.tsx

--- src/components/formcomponents/date/__tests__/__data__/time/index.ts
+++ src/components/formcomponents/date/__tests__/__data__/time/index.ts
@@ -2,6 +2,6 @@ import * as fs from 'fs';
 import { Questionnaire } from 'fhir/r4';
 
 const q: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q.json').toString());
-const qMinMax: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-minMax.json').toString());
-const qMinMaxCustomError: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-minMax-customError.json').toString());
-export { q, qMinMax, qMinMaxCustomError };
+// const qMinMax: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-minMax.json').toString());
+// const qMinMaxCustomError: Questionnaire = JSON.parse(fs.readFileSync(__dirname + '/q-minMax-customError.json').toString());
+export { q };
