4daabea442e4f09e778d2264517d6d4bbe6dddb6
rinrub <99617143+rinrub@users.noreply.github.com>
2024-08-19 22:25:48 +0200

    global this

--- test/setup-test.ts
+++ test/setup-test.ts
@@ -8,19 +8,6 @@ import './__mocks__/IntersectionObserver';
 // import './__mocks__/useSize';
 // import './__mocks__/useHover';
 // import './__mocks__/usePseudoClasses';
-Object.defineProperty(globalThis, 'IS_REACT_ACT_ENVIRONMENT', {
-  get() {
-    if (typeof globalThis.self !== 'undefined') {
-      return globalThis.self.IS_REACT_ACT_ENVIRONMENT;
-    }
-  },
-  set(value) {
-    if (typeof globalThis.self !== 'undefined') {
-      globalThis.self.IS_REACT_ACT_ENVIRONMENT = value;
-    }
-  },
-});
-
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 Object.defineProperty(window, 'scrollTo', {
   value: vi.fn(),
