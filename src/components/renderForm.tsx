40d7a3fe7ebc1ea9520fc853a42420c1763f33e9
Reuben Ringdal <rubenr@aboveit.no>
2024-09-17 10:21:23 +0200

    small fixes

--- src/components/renderForm.tsx
+++ src/components/renderForm.tsx
@@ -47,7 +47,6 @@ const RenderForm = ({
   const onSubmitReactHookForm: SubmitHandler<FieldValues> = (): void => {
     onSubmit();
   };
-  const { errors } = methods.formState;
   const displayPauseButtonInNormalView = referoProps.onSave ? onSave : undefined;
   const displayPauseButtonInStepView = displayPreviousButton ? previousStep : undefined;
   const displayValidationSummaryOnTop: boolean =
