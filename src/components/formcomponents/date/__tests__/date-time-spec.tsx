27ff233e9d5b5be370199ce55de35aa599cc5613
Reuben Ringdal <rubenr@aboveit.no>
2024-09-20 13:16:13 +0200

    remove safeparse for dates

--- src/components/formcomponents/date/__tests__/date-time-spec.tsx
+++ src/components/formcomponents/date/__tests__/date-time-spec.tsx
@@ -1,5 +1,5 @@
 import { Questionnaire, QuestionnaireResponseItemAnswer } from 'fhir/r4';
-import { act, findByRole, renderRefero, userEvent, waitFor } from '@test/test-utils.tsx';
+import { findByRole, renderRefero, userEvent, waitFor } from '@test/test-utils.tsx';
 import { q, qMinMax, qMinMaxCustomError } from './__data__/date-time';
 import { ReferoProps } from '../../../../types/referoProps';
 import { Extensions } from '../../../../constants/extensions';
--- src/components/formcomponents/table/tables/constants.ts
+++ src/components/formcomponents/table/tables/constants.ts
@@ -2,4 +2,4 @@ export const DATEFORMATS = {
   DATE: 'dd.MM.yyyy',
   DATETIME: 'dd MM yyyy HH:mm',
   TIME: 'HH:mm',
-};
+} as const;
