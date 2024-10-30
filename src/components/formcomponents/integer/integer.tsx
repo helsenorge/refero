04e936ce668c6315fdd89959dca373f92b879d36
Reuben Ringdal <rubenr@aboveit.no>
2024-10-30 11:15:31 +0100

    validation numbers

--- src/components/formcomponents/integer/integer.tsx
+++ src/components/formcomponents/integer/integer.tsx
@@ -85,6 +85,7 @@ const Integer = (props: Props): JSX.Element | null => {
     min: minValue({ item, resources }),
     shouldUnregister: true,
   };
+  console.log(validationRules.min);
   const { onChange, ...rest } = register(idWithLinkIdAndItemIndex, shouldValidate(item, pdf) ? validationRules : undefined);
 
   if (pdf || isReadOnly(item)) {
