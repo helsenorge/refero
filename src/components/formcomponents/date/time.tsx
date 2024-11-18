d37de4dd9ba686f4b5fb15fdc2cd3d3fcef5f7cb
anagelpe <andreas.nagel-petersen@nhn.no>
2024-11-18 15:36:13 +0100

    Fixed leading zero in time component

--- src/components/formcomponents/date/time.tsx
+++ src/components/formcomponents/date/time.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { newTimeValueAsync } from '../../../actions/newValue';
 import { getId, isReadOnly, isRequired } from '../../../util/index';
@@ -10,7 +10,6 @@ import { useSelector } from 'react-redux';
 
 import { GlobalState, useAppDispatch } from '@/reducers';
 import RenderDeleteButton from '../repeat/RenderDeleteButton';
-import { DateTime, DateTimePickerWrapper } from '@helsenorge/datepicker/components/DatePicker';
 import styles from '../common-styles.module.css';
 import { FieldError, FieldValues, RegisterOptions, useFormContext } from 'react-hook-form';
 import {
@@ -30,28 +29,37 @@ import useOnAnswerChange from '@/hooks/useOnAnswerChange';
 import FormGroup from '@helsenorge/designsystem-react/components/FormGroup';
 import { ReadOnly } from '../read-only/readOnly';
 import { shouldValidate } from '@/components/validation/utils';
-import { getErrorMessage, isNumber } from '@/components/validation/rules';
+import { getErrorMessage } from '@/components/validation/rules';
 import { TimeUnit } from '@/types/dateTypes';
 import { useResetFormField } from '@/hooks/useResetFormField';
+import dateStyles from './date.module.css';
+import Input from '@helsenorge/designsystem-react/components/Input';
+import Label from '@helsenorge/designsystem-react/components/Label';
+import SafeText from '@/components/referoLabel/SafeText';
 
 export type Props = QuestionnaireComponentItemProps;
 
 const Time = ({ id, index, path, linkId, pdf, idWithLinkIdAndItemIndex, children }: Props): JSX.Element | null => {
   initialize();
-  const item = useSelector<GlobalState, QuestionnaireItem | undefined>(state => findQuestionnaireItem(state, linkId));
 
+  const item = useSelector<GlobalState, QuestionnaireItem | undefined>(state => findQuestionnaireItem(state, linkId));
+  const { formState, getFieldState, setValue, getValues, trigger, register } = useFormContext<FieldValues>();
   const { promptLoginMessage, globalOnChange, resources } = useExternalRenderContext();
   const onAnswerChange = useOnAnswerChange(globalOnChange);
   const dispatch = useAppDispatch();
-  const { formState, getFieldState, setValue, getValues, trigger, register } = useFormContext<FieldValues>();
+
   const hoursField = getFieldState(`${idWithLinkIdAndItemIndex}-hours`, formState);
   const minutesField = getFieldState(`${idWithLinkIdAndItemIndex}-minutes`, formState);
   const answer = useGetAnswer(linkId, path);
   const hoursAndMinutesFromAnswer = extractHoursAndMinutesFromAnswer(answer, item);
   const hours = hoursAndMinutesFromAnswer?.hours;
   const minutes = hoursAndMinutesFromAnswer?.minutes;
+  const [hoursValue, setHoursValue] = useState(hours);
+  const [minutesValue, setMinutesValue] = useState(minutes);
+
   useResetFormField(`${idWithLinkIdAndItemIndex}-hours`, hours);
   useResetFormField(`${idWithLinkIdAndItemIndex}-minutes`, minutes);
+
   const getTimeValueFromAnswer = (): string | undefined => {
     if (!answer) return undefined;
 
@@ -78,37 +86,42 @@ const Time = ({ id, index, path, linkId, pdf, idWithLinkIdAndItemIndex, children
     }
   };
 
-  const handleHoursChange = (newHours: number | string | undefined): void => {
-    let newMinutes: number | string | undefined = minutes;
+  const handleHoursChange = (newHours: string | undefined): void => {
+    let newMinutes: string | undefined = minutes;
     if (!minutes) {
       newMinutes = '00';
     }
-    updateQuestionnaireResponse(newHours?.toString(), newMinutes?.toString());
+    let newString = newHours === '' ? undefined : newHours;
+    if (newHours && Number(newHours) >= 0 && Number(newHours) < 24) {
+      newString = newHours;
+    }
+    setHoursValue(newString);
+    updateQuestionnaireResponse(newHours, newMinutes);
   };
-  const handleMinutesChange = (newMinutes: number | string | undefined): void => {
+
+  const handleMinutesChange = (newMinutes: string | undefined): void => {
     //trigger validations in the hour field as well, to validate if time is valid
     if (formState.isSubmitted) {
       trigger(idWithLinkIdAndItemIndex + '-hours');
     }
-    let newHours: number | string | undefined = hours;
+
+    let newHours: string | undefined = hours;
     if (!hours) {
       newHours = '00';
     }
-    updateQuestionnaireResponse(newHours?.toString(), newMinutes?.toString());
+    let newString = newMinutes === '' ? undefined : newMinutes;
+    if (newMinutes && Number(newMinutes) >= 0 && Number(newMinutes) < 60) {
+      newString = newMinutes;
+    }
+    setMinutesValue(newString);
+    updateQuestionnaireResponse(newHours, newMinutes);
   };
 
   const makeValidTime = (hours: string | undefined, minutes: string | undefined): string => {
     const paddedHours = hours?.padStart(2, '0');
     const paddedMinutes = minutes?.padStart(2, '0');
 
-    return addSeconds(`${paddedHours}:${paddedMinutes}`);
-  };
-
-  const addSeconds = (time: string): string => {
-    if (time && time.split(':').length === 2) {
-      return `${time}:00`;
-    }
-    return time;
+    return `${paddedHours}:${paddedMinutes}:00`;
   };
 
   function getCombinedFieldError(hoursField: FieldValues, minutesField: FieldValues): FieldError | undefined {
@@ -196,30 +209,35 @@ const Time = ({ id, index, path, linkId, pdf, idWithLinkIdAndItemIndex, children
           testId={`${getId(id)}-label-test`}
           sublabelId={`${getId(id)}-sublabel`}
         />
-        <DateTimePickerWrapper>
-          <DateTime
+        <div className={dateStyles.timeWrapper}>
+          <Input
             {...restHours}
             inputId={`${getId(id)}-datetime-hours`}
+            type="number"
             testId={`time-1`}
-            value={isNumber(hours) ? Number(hours) : undefined}
-            timeUnit="hours"
             onChange={e => {
               handleHoursChange(e.target.value);
               onChangeHours(e);
             }}
+            width={4}
+            value={hoursValue}
           />
-
-          <DateTime
+          <Label labelTexts={[]} className={dateStyles.timeColon}>
+            <SafeText as="span" text={':'}></SafeText>
+          </Label>
+          <Input
             {...restMinutes}
+            type="number"
             testId={`time-2`}
-            value={isNumber(minutes) ? Number(minutes) : undefined}
-            timeUnit="minutes"
             onChange={e => {
               handleMinutesChange(e.target.value);
               onChangeMinutes(e);
             }}
+            width={4}
+            value={minutesValue}
           />
-        </DateTimePickerWrapper>
+        </div>
+
         <RenderDeleteButton item={item} path={path} index={index} className="page_refero__deletebutton--margin-top" />
         <RenderRepeatButton path={path} item={item} index={index} />
         {children ? <div className="nested-fieldset nested-fieldset--full-height">{children}</div> : null}
