50611e29f98091f045faeaec52a13c2dc32fcab8
Reuben Ringdal <rubenr@aboveit.no>
2024-12-02 23:07:18 +0100

    update genereate response function

--- src/actions/generateQuestionnaireResponse.ts
+++ src/actions/generateQuestionnaireResponse.ts
@@ -4,7 +4,9 @@ import Constants from '../constants/index';
 import itemType from '../constants/itemType';
 import StatusConstants from '../constants/status';
 import { createQuestionnaireResponseAnswer } from '../util/createQuestionnaireResponseAnswer';
-import { getMinOccursExtensionValue } from '../util/extension';
+import { getCalculatedExpressionExtension, getCopyExtension, getMinOccursExtensionValue } from '../util/extension';
+import { evaluateFhirpathExpressionToGetString } from '@/util/fhirpathHelper';
+import ItemType from '../constants/itemType';
 
 export function generateQuestionnaireResponse(questionnaire: Questionnaire): QuestionnaireResponse | undefined {
   const response: QuestionnaireResponse = {
@@ -31,7 +33,7 @@ export function generateQuestionnaireResponse(questionnaire: Questionnaire): Que
       response.item.push(responseItem);
     }
   });
-  return response;
+  return evaluateCalculatedExpressions(questionnaire, response);
 }
 
 function addChildrenItemsToResponseItem(item: QuestionnaireItem, response: QuestionnaireResponseItem): void {
@@ -98,4 +100,72 @@ export function createQuestionnaireResponseItem(item: QuestionnaireItem): Questi
   }
 
   return responseItem;
+}
+function evaluateCalculatedExpressions(questionnaire: Questionnaire, response: QuestionnaireResponse): QuestionnaireResponse {
+  function traverseItems(qItems: QuestionnaireItem[], qrItems: QuestionnaireResponseItem[]): void {
+    qItems.forEach((qItem, index) => {
+      const qrItem = qrItems[index];
+      const expressionToEvaluate = getCopyExtension(qItem) ?? getCalculatedExpressionExtension(qItem);
+      if (expressionToEvaluate && expressionToEvaluate.valueString) {
+        const result = evaluateFhirpathExpressionToGetString(expressionToEvaluate, response);
+        if (result.length > 0) {
+          const calculatedValue = result[0];
+          const answer = qrItem.answer ? qrItem.answer[0] : {};
+          switch (qItem.type) {
+            case ItemType.BOOLEAN:
+              answer.valueBoolean = Boolean(calculatedValue);
+              break;
+            case ItemType.DECIMAL:
+              answer.valueDecimal = Number(calculatedValue);
+              break;
+            case ItemType.INTEGER:
+              answer.valueInteger = Number(calculatedValue);
+              break;
+            case ItemType.QUANTITY:
+              answer.valueQuantity = calculatedValue;
+              break;
+            case ItemType.DATE:
+              answer.valueDate = String(calculatedValue);
+              break;
+            case ItemType.DATETIME:
+              answer.valueDateTime = String(calculatedValue);
+              break;
+            case ItemType.TIME:
+              answer.valueTime = String(calculatedValue);
+              break;
+            case ItemType.TEXT:
+            case ItemType.STRING:
+              answer.valueString = String(calculatedValue);
+              break;
+            case ItemType.ATTATCHMENT:
+              answer.valueAttachment = calculatedValue;
+              break;
+            case ItemType.CHOICE:
+            case ItemType.OPENCHOICE:
+              answer.valueCoding = calculatedValue;
+              break;
+            default:
+              if (typeof answer === 'string') {
+                (answer as { valueString: string }).valueString = calculatedValue;
+              } else {
+                answer.valueCoding = calculatedValue;
+              }
+              break;
+          }
+          qrItem.answer = [answer];
+        }
+      }
+
+      // Recursively process child items
+      if (qItem.item && qrItem.item) {
+        traverseItems(qItem.item, qrItem.item);
+      }
+    });
+  }
+
+  if (questionnaire.item && response.item) {
+    traverseItems(questionnaire.item, response.item);
+  }
+
+  return response; // Return the updated response
 }
