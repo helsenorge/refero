f3df1743c2227cc70d73155647a50a11b8be1f46
Reuben Ringdal <rubenr@aboveit.no>
2024-12-04 15:03:39 +0100

    fix tests and split scoring

--- src/components/formcomponents/repeat/RepeatButton.tsx
+++ src/components/formcomponents/repeat/RepeatButton.tsx
@@ -4,11 +4,12 @@ import Button from '@helsenorge/designsystem-react/components/Button';
 import Icon from '@helsenorge/designsystem-react/components/Icon';
 import PlusLarge from '@helsenorge/designsystem-react/components/Icons/PlusLarge';
 
-import { addRepeatItemAction } from '../../../actions/newValue';
+import { addRepeatItemAsync } from '../../../actions/newValue';
 import { useAppDispatch } from '../../../reducers';
 import { getRepeatsTextExtension } from '../../../util/extension';
 import { Path } from '../../../util/refero-core';
 import { useExternalRenderContext } from '@/context/externalRenderContext';
+import useOnAnswerChange from '@/hooks/useOnAnswerChange';
 
 interface Props {
   item?: QuestionnaireItem;
@@ -20,9 +21,12 @@ interface Props {
 export const RepeatButton = ({ item, parentPath, responseItems, disabled }: Props): JSX.Element => {
   const dispatch = useAppDispatch();
   const { resources } = useExternalRenderContext();
+  const onAnswerChange = useOnAnswerChange();
   const onAddRepeatItem = (): void => {
     if (dispatch && item) {
-      dispatch(addRepeatItemAction({ parentPath, item, responseItems }));
+      dispatch(addRepeatItemAsync(parentPath, item, responseItems))?.then(newState => {
+        return onAnswerChange && onAnswerChange(newState, item);
+      });
     }
   };
   const text = getRepeatsTextExtension(item);
