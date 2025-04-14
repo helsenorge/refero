54e6f5afd7f683fc654d23290def2cea80927622
ruben <rubenr@aboveit.no>
2025-04-14 19:03:19 +0200

    fix bug

--- src/components/formcomponents/text/text.tsx
+++ src/components/formcomponents/text/text.tsx
@@ -91,6 +91,7 @@ export const Text = (props: Props): JSX.Element | null => {
     pattern: regexpPattern({ item, resources }),
     validate: (value: string): string | true | undefined =>
       scriptInjection({ value, resources, shouldValidate: !!validateScriptInjection }),
+    setValueAs: (value: string) => value?.trim(),
     shouldUnregister: true,
   };
 
