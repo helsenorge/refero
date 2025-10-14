46d0340a4c23098b419eba6c44c21232423118dd
ruben <rubenr@aboveit.no>
2025-10-14 14:39:41 +0200

    import helper functions form correct location in enableWhen_new

--- src/workers/helpers.ts
+++ src/workers/helpers.ts
@@ -1,9 +1,20 @@
 import type { Questionnaire, QuestionnaireResponse } from 'fhir/r4';
 
+import { ClearAction } from '@/actions/newValue';
+import { runEnableWhenNew } from '@/calculators/runEnableWhen_new';
 import { FhirPathExtensions, type AnswerPad } from '@/util/FhirPathExtensions';
 
 export function runCalculators(args: { questionnaireResponse: QuestionnaireResponse; questionnaire: Questionnaire }): AnswerPad {
   const engine = new FhirPathExtensions(args.questionnaire);
   const updated = engine.evaluateAllExpressions(args.questionnaireResponse);
   return engine.calculateFhirScore(updated);
+}
+export function runEnableWhen({
+  questionnaireResponse,
+  questionnaire,
+}: {
+  questionnaireResponse: QuestionnaireResponse | null | undefined;
+  questionnaire: Questionnaire | null | undefined;
+}): ClearAction[] {
+  return runEnableWhenNew({ questionnaire, questionnaireResponse });
 }
